f763889bd8d6ae8336daa69427cede4cc9369821
DM <deifamulus@gmail.com>
2016-04-23 14:17:54 -0500

    stuff. debugging etc

--- memoryClasses.py
+++ memoryClasses.py
@@ -10,8 +10,8 @@ class MemoryBank:
 	def addSegment(self,segment,note=''):
 		address=self.findAddress(segment)
 		if address==-1:
-			print 'couldn\'t find a spot for this segment:'
-			print (segment.size,segment.align)
+			#print 'couldn\'t find a spot for this segment:'
+			#print (segment.size,segment.align)
 			return
 		if segment.align != 1:
 			note='align:{0}'.format(segment.align)
@@ -45,7 +45,47 @@ class MemoryBank:
 					self.freeSpaces.append([newAddress+size,oldEnd])
 		self.freeSpaces.sort(key=lambda k: k[0])
 		# print self.freeSpaces
-			
+	def createFreeSpaceSegs(self,memBank):
+		freeSegs = []
+		for space in memBank.freeSpaces:
+			freeSegs.append(MemorySegment(((space[1]-space[0])+1),1))
+		for seg in freeSegs:
+			memBank.addSegment(seg,'free')
+		
+class MemoryBankOS:
+	blocks = []
+	freeSpace=[]
+	def __init__(self,inpsize=65536):
+		self.size=inpsize
+		self.freeSpace=[0,inpsize-1]
+
+	def addSegment(self,segment,note=''):
+		address=self.findAddress(segment)
+		if address==-1:
+			#print 'couldn\'t find a spot for this segment:'
+			#print (segment.size,segment.align)
+			return
+		if segment.align != 1:
+			note='align:{0}'.format(segment.align)
+		if len(self.blocks)>0:
+			firstFreeAddress=(self.blocks[-1][0]+self.blocks[-1][1])
+			if (firstFreeAddress<address):
+				self.blocks.append([firstFreeAddress,address-firstFreeAddress,'free'])
+		self.blocks.append([address,segment.size,note])
+		self.freeSpace[0]=address+segment.size
+		self.blocks.sort(key=lambda k: k[0])
+		
+	def findAddress(self,segment):
+		newAddress=-1
+		# print 'trying to find ', segment.size, 'addressses'
+		theorStartAddr = ((self.freeSpace[0]-1)+(segment.align - (self.freeSpace[0]-1)%segment.align))
+		theorEndAddr = theorStartAddr+segment.size-1
+		# print 'theoretical start address = ', theorStartAddr
+		# print 'theoretical end address = ', theorEndAddr
+		# print 'max end address = ', space[1]
+		if (theorEndAddr<=self.freeSpace[1]):
+			newAddress = theorStartAddr
+		return newAddress			
 
 class MemorySegment:
 	def __init__(self,size,alignment):
--- memorypacker.py
+++ memorypacker.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 
-import sys, getopt
+import sys, getopt, time
 import memoryClasses
 import memoryGenerator
 
@@ -12,37 +12,67 @@ def formatInput(inputfile):
 		splitlist.append(line.split())
 	intsplitlist = [map(int,x) for x in splitlist]
 	sortedlist = intsplitlist.sort(key = lambda k: (k[0], k[1]), reverse=True)
+	infile.close()
 	return intsplitlist
-
-def createFreeSpaceSegs(memBank):
-	freeSegs = []
-	for space in memBank.freeSpaces:
-		freeSegs.append(memoryClasses.MemorySegment(((space[1]-space[0])+1),1))
-	for seg in freeSegs:
-		memBank.addSegment(seg,'free')
 	
 def fillMemory(infile, outfile, memsize):
-	print 'Filling Memory'
+	#print 'Filling Memory'
+	
 	formattedInput = formatInput(infile)
 	memBank=memoryClasses.MemoryBank(memsize)
 	segments=[]
 	for pair in formattedInput:
 		segments.append(memoryClasses.MemorySegment(pair[0],pair[1]))
+	#totaltimesegs=0
+	# SLOWING DOWN
+	#sys.stdout.write('before loop: {0}\n'.format(time.clock()))
+	for seg in segments:
+		#timestart=time.clock()
+		memBank.addSegment(seg)
+		#totaltimesegs+=time.clock()-timestart
+	# SLOWING DOWN
+	#timeperseg=totaltimesegs/len(segments)
+	# sys.stdout.write('after loop: {0}\n'.format(time.clock()))
+	# sys.stdout.write('time per segment: {0}\n'.format(timeperseg))
+	# sys.stdout.flush()
+	memBank.createFreeSpaceSegs(memBank)
+	if outfile != '':
+		outfp=open(outfile,'w')
+		outfp.write('Address,Size,Note\n')
+		for block in memBank.blocks:
+			for item in block:
+				outfp.write('%s,' % item)
+			outfp.write('\n')
+		outfp.close()
+	freespace = sum([block[1] for block in memBank.blocks if block[2]=='free'])
+	return freespace
+	
+def fillMemoryOS(infile, outfile, memsize):
+	#print 'Filling Memory (OS Algorithm)'
+	formattedInput = formatInput(infile)
+	memBank=memoryClasses.MemoryBankOS(memsize)
+	segments=[]
+	for pair in formattedInput:
+		segments.append(memoryClasses.MemorySegment(pair[0],pair[1]))	
 	for seg in segments:
 		memBank.addSegment(seg)
-	createFreeSpaceSegs(memBank)
-	outfp=open(outfile,'w')
-	outfp.write('Address,Size,Note\n')
-	for block in memBank.blocks:
-		for item in block:
-			outfp.write('%s,' % item)
-		outfp.write('\n')
+	if outfile != '':
+		outfp=open(outfile,'w')
+		outfp.write('Address,Size,Note\n')
+		for block in memBank.blocks:
+			for item in block:
+				outfp.write('%s,' % item)
+			outfp.write('\n')
+		outfp.close()
+	freespace = sum([block[1] for block in memBank.blocks if block[2]=='free'])
+	return freespace	
 	
 def main(argv):
 	inputfile = ''
 	outputfile = ''
 	memsize = 16
-	opts, args = getopt.getopt(argv,'hi:o:s:',['ifile=','ofile=','memsize='])
+	testmode=0
+	opts, args = getopt.getopt(argv,'hi:o:s:t:',['ifile=','ofile=','memsize=','test='])
 	for opt, arg in opts:
 		if opt == '-h':
 			print 'test.py -i <inputfile> -o <outputfile> -s <memsize>'
@@ -53,10 +83,45 @@ def main(argv):
 			outputfile = arg
 		elif opt in ('-s', '--memsize'):
 			memsize = int(arg)
-	if inputfile=='':
+		elif opt in ('-t', '--test'):
+			testmode = int(arg)
+	if testmode==0:
+		if inputfile=='':
+			inputfile='genInp.txt'
+			memoryGenerator.createSegments(memsize)
+		fillMemory(inputfile, outputfile, memsize)
+	else:
 		inputfile='genInp.txt'
-		memoryGenerator.createSegments(memsize)
-	fillMemory(inputfile, outputfile, memsize)	
+		outputfile=''
+		totalUnused=0
+		unusedSpacePercent=0
+		totalUnusedOS=0
+		unusedSpaceOSPercent=0
+		for i in range(testmode):
+			sys.stdout.write('test {0:5}/{1:5}, {2:3f}%\r'.format(i,testmode,(i/float(testmode)*100)))
+			sys.stdout.flush()
+			#sys.stdout.write('before input generation: {0}\n'.format(time.clock()))
+			#sys.stdout.flush()
+			memoryGenerator.createSegments(memsize)
+			#sys.stdout.write('before my algorithm: {0}\n'.format(time.clock()))
+			#sys.stdout.flush()
+			unusedSpace=fillMemory(inputfile,outputfile,memsize)
+			#sys.stdout.write('before my percentage calc: {0}\n'.format(time.clock()))
+			#sys.stdout.flush()
+			totalUnused+=unusedSpace
+			unusedSpacePercent=(unusedSpacePercent*(i)+unusedSpace/float(memsize)*100)/(i+1)
+			#sys.stdout.write('before OS\'s algorithm: {0}\n'.format(time.clock()))
+			#sys.stdout.flush()
+			unusedSpaceOS=fillMemoryOS(inputfile,outputfile,memsize)
+			#sys.stdout.write('before OS percentage calc: {0}\n'.format(time.clock()))
+			#sys.stdout.flush()
+			totalUnusedOS+=unusedSpaceOS
+			unusedSpaceOSPercent=(unusedSpaceOSPercent*(i)+unusedSpaceOS/float(memsize)*100)/(i+1)
+
+		print 'Unused Space (my Algorithm): {0}, {1:3f}%'.format(totalUnused,unusedSpacePercent)
+		print 'Unused Space (OS Algorithm): {0}, {1:3f}%'.format(totalUnusedOS, unusedSpaceOSPercent)
+		with open('unusedSpace.csv','a') as totalfile:
+			totalfile.write('{0},{1},{2}\n'.format(totalUnused,totalUnusedOS,memsize*testmode))
    
 if __name__ == '__main__':
    main(sys.argv[1:])
