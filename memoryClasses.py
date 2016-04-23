7c1885f3c4befd0a0e66bb61073398767c85c0ad
DM <deifamulus@gmail.com>
2016-04-23 08:48:17 -0500

    output and formatting

--- memoryClasses.py
+++ memoryClasses.py
@@ -7,25 +7,25 @@ class MemoryBank:
 		
 
 		
-	def addSegment(self,segment):
+	def addSegment(self,segment,note=''):
 		address=self.findAddress(segment)
 		if address==-1:
-			print "couldn't find a spot for this segment:"
+			print 'couldn\'t find a spot for this segment:'
 			print (segment.size,segment.align)
 			return
-		self.blocks.append([address,segment.size])
+		self.blocks.append([address,segment.size,note])
 		self.updateFreeSpace(address,segment.size)
 		self.blocks.sort(key=lambda k: k[0])
 		
 	def findAddress(self,segment):
 		newAddress=-1
-		# print "trying to find ", segment.size, "addressses"
+		# print 'trying to find ', segment.size, 'addressses'
 		for space in self.freeSpaces:
 			theorStartAddr = ((space[0]-1)+(segment.align - (space[0]-1)%segment.align))
 			theorEndAddr = theorStartAddr+segment.size-1
-			# print "theoretical start address = ", theorStartAddr
-			# print "theoretical end address = ", theorEndAddr
-			# print "max end address = ", space[1]
+			# print 'theoretical start address = ', theorStartAddr
+			# print 'theoretical end address = ', theorEndAddr
+			# print 'max end address = ', space[1]
 			if (theorEndAddr<=space[1]):
 				newAddress = theorStartAddr
 				break
--- memorypacker.py
+++ memorypacker.py
@@ -12,9 +12,16 @@ def formatInput(inputfile):
 	intsplitlist = [map(int,x) for x in splitlist]
 	sortedlist = intsplitlist.sort(key = lambda k: (k[0], k[1]), reverse=True)
 	return intsplitlist
+
+def createFreeSpaceSegs(memBank):
+	freeSegs = []
+	for space in memBank.freeSpaces:
+		freeSegs.append(memoryClasses.MemorySegment(((space[1]-space[0])+1),1))
+	for seg in freeSegs:
+		memBank.addSegment(seg,'free')
 	
 def fillMemory(infile, outfile, memsize):
-	print "Filling Memory"
+	print 'Filling Memory'
 	formattedInput = formatInput(infile)
 	memBank=memoryClasses.MemoryBank(memsize)
 	segments=[]
@@ -22,16 +29,20 @@ def fillMemory(infile, outfile, memsize):
 		segments.append(memoryClasses.MemorySegment(pair[0],pair[1]))
 	for seg in segments:
 		memBank.addSegment(seg)
+	createFreeSpaceSegs(memBank)
+	outfp=open(outfile,'w')
+	outfp.write('Address,Size,Note\n')
 	for block in memBank.blocks:
-		print block
-	print memBank.freeSpaces
+		for item in block:
+			outfp.write('%s,' % item)
+		outfp.write('\n')
 	
 def main(argv):
 	inputfile = ''
 	outputfile = ''
 	memsize = 16
 	try:
-		opts, args = getopt.getopt(argv,"hi:o:s:",["ifile=","ofile=","memsize="])
+		opts, args = getopt.getopt(argv,'hi:o:s:',['ifile=','ofile=','memsize='])
 	except getopt.GetoptError:
 		print 'test.py -i <inputfile> -o <outputfile> -s <memsize>'
 		sys.exit(2)
@@ -39,13 +50,13 @@ def main(argv):
 		if opt == '-h':
 			print 'test.py -i <inputfile> -o <outputfile> -s <memsize>'
 			sys.exit()
-		elif opt in ("-i", "--ifile"):
+		elif opt in ('-i', '--ifile'):
 			inputfile = arg
-		elif opt in ("-o", "--ofile"):
+		elif opt in ('-o', '--ofile'):
 			outputfile = arg
-		elif opt in ("-s", "--memsize"):
+		elif opt in ('-s', '--memsize'):
 			memsize = int(arg)
 	fillMemory(inputfile, outputfile, memsize)	
    
-if __name__ == "__main__":
+if __name__ == '__main__':
    main(sys.argv[1:])
